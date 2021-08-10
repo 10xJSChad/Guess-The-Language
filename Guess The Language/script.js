c3d5b2e770ed918274574e41406bac88f4faa828
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-08-10 04:01:20 +0200

    Add files via upload

--- Guess The Language/script.js
+++ Guess The Language/script.js
@@ -1,19 +1,43 @@
-var supportedLanguages = ["English", "German", "Spanish", "Icelandic"];
+//Elements
+var questionText;
+var answerButtons;
+var scoreText;
+
+//Languages & questions
+var supportedLanguages = ["English", "German", "Spanish", "Icelandic", "Swedish", "Norwegian", "Portuguese", "Italian"];
 var questionsList = [
+    ["Swedish", "Jag vill känna hur slugheten får min hjärna att arbeta"],
+    ["Swedish", "Vem har väl förlitat sig på själva skadan"],
+    ["Swedish", "Det finns en motvind som jag aldrig känt"],
+    ["Norwegian", "Hastverk er ikke bra for Dem. De har god tid! Ta det med ro, nyt dagen og les noen dikt."],
+    ["Norwegian", "Bønder sine økser brynte hvor en hær dro frem"],
+    ["Icelandic", "Blíndur er bóklaus ma∂ur."],
+    ["Icelandic", "Risastór stjarna, miklu stærri en við getum ímyndað okkur"],
+    ["Italian", "Si dice sempre il lupo più grande che non è."],
+    ["Italian", "La famiglia è la patria del cuore. "],
+    ["Portuguese", "Quem vê cara não vê coração"],
+    ["Portuguese", "Mais vale um pássaro na mão do que dois voando"],
+    ["Portuguese", "Cão que ladra não morde"], 
     ["English", "It is a far, far better thing that I do, than I have ever done; it is a far, far better rest I go to than I have ever known"],
     ["English", "All we have to decide is what to do with the time that is given us."],
     ["German", "Nur die Harten kommen in den Garten"],
     ["German", "Alles hat ein Ende, nur die Wurst hat zwei"],
-    ["Spanish", "Que si las manos son nuestras, es nuestro lo que nos den"]
+    ["Spanish", "Que si las manos son nuestras, es nuestro lo que nos den"],
+    ["Spanish", "No me pagen porque cante"],
+    ["Spanish", "Escucha, yo vengo a cantar"],
+    ["Spanish", "Papel contra balas, no puede servir"]
 ];
 
 var questionAndAnswer = ["bunghole", 2];
-var questionText;
-var answerButtons;
+
+//Gameplay variables
+var correct = 0;
+var incorrect = 0;
 
 function getElements(){
-    questionText = document.getElementById("questionText");
+    questionText = document.getElementsByClassName("question-text")[0];
     answerButtons = document.getElementsByClassName("answer-button");
+    scoreText = document.getElementsByClassName("score-text")[0];
 }
 
 function shuffleList(list){
@@ -57,6 +81,10 @@ function updateButtons(){
     }
 }
 
+function updateText(){
+    scoreText.innerHTML = ("Correct: " + correct + " | Incorrect: " + incorrect)
+}
+
 function greyOutButtonByAnswer(answer){
     for(i = 0; i<answerButtons.length;i++){
         if(answerButtons[i].value == answer) answerButtons[i].disabled = true;
@@ -64,8 +92,10 @@ function greyOutButtonByAnswer(answer){
 }
 
 function giveAnswer(answer){
-    if(answer == questionAndAnswer[0]) createQuestion();
-    else greyOutButtonByAnswer(answer);
+    if(answer == questionAndAnswer[0]) {createQuestion(); correct++;}
+    else {greyOutButtonByAnswer(answer); incorrect++;}
+
+    updateText();
 }
 
 function answerCorrect(){
